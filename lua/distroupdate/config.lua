78b9ac9d12554e6f0dfa990125055de88c2dbbd5
Zeioth <zeioth@hotmail.com>
2024-02-09 00:46:32 +0100

    chore: Order config options alphabetically.

--- lua/distroupdate/config.lua
+++ lua/distroupdate/config.lua
@@ -6,13 +6,13 @@ local utils = require("distroupdate.utils")
 ---Parse user options, or set the defaults
 ---@param opts table A table with options to set.
 function M.set(opts)
-  M.remote = opts.remote or "origin"
   M.channel = opts.channel or "stable"
-  M.snapthot_file = opts.snapthot_file or utils.os_path(vim.fn.stdpath "config" .. "/lua/lazy_snapshot.lua")
-  M.snapthot_module = opts.snapthot_module or "lazy_snapshot"
-  M.rollback_file = opts.rollback_file or  utils.os_path(vim.fn.stdpath "cache" .. "/rollback.lua")
-  M.release_tag = opts.release_tag or ""
+  M.hot_reload_exta_behavior = opts.hot_reload_exta_behavior or function() end
   M.hot_reload_files = opts.hot_reload_files or {}
+  M.release_tag = opts.release_tag or nil
+  M.remote = opts.remote or "origin"
+  M.rollback_file = opts.rollback_file or  utils.os_path(vim.fn.stdpath "cache" .. "/rollback.lua")
+  M.snapshot_file = opts.snapshot_file or utils.os_path(vim.fn.stdpath "config" .. "/lua/lazy_snapshot.lua")
 
   -- expose the config as global
   vim.g.distroupdate_config = M
