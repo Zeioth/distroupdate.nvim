afc62c21a6f0eb11a1ad44fffb2748228565ee40
Zeioth <Zeioth@hotmail.com>
2024-09-24 14:25:40 +0200

    comments

--- lua/distroupdate/utils/init.lua
+++ lua/distroupdate/utils/init.lua
@@ -15,10 +15,9 @@ local M = {}
 
 --- Run a shell command and capture the output and if the command
 --- succeeded or failed
----@param cmd string|string[] The terminal command to execute
----@param show_error? boolean Whether or not to show an unsuccessful command
----                           as an error to the user
----@return string|nil # The result of a successfully executed command or nil
+---@param cmd string|string[] The terminal command to execute.
+---@param show_error? boolean If true, print errors if the command fail.
+---@return string|nil # The result of a successfully executed command or nil.
 function M.cmd(cmd, show_error)
   if type(cmd) == "string" then cmd = vim.split(cmd, " ") end
   if vim.fn.has("win32") == 1 then cmd = vim.list_extend({ "cmd.exe", "/C" }, cmd) end
